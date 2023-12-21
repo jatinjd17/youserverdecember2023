633a1ac07630f424273074e090993d6910889280
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-12-21 16:33:38 +0530

    latest changes

--- api/server.py
+++ api/server.py
@@ -86,5 +86,5 @@ def getdata():
         data = {}
     return data
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5000)
